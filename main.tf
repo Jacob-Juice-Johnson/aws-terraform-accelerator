2d84bd5200f73e7005597df60ebaf2fa1d254cc9
Jacob Johnson <jacob.johnson@slalom.com>
2023-01-26 15:13:21 -0600

    Adds bool to test var inject

--- main.tf
+++ main.tf
@@ -7,3 +7,15 @@ module "s3" {
     }
   )
 }
+
+# Module to test variable inject from POST call for a self service IaC TF workflow
+module "s3_test_var_inject" {
+  count       = var.test_var_inject == true ? 1 : 0
+  source      = "./modules/s3"
+  name_prefix = "${var.tags.project}-${var.tags.env}-${var.tags.region}-var-inject"
+  tags = merge(var.tags,
+    {
+      module = "s3_test_var_inject"
+    }
+  )
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "tags" {
   type        = map(any)
   description = "key value pairs for resources created via terraform"
 }
+
+variable "test_var_inject" {
+  type        = bool
+  description = "Test var inject conecept"
+  default     = false
+}
